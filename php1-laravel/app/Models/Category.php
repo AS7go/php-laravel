a15fc75470c7aa23250c8e3872cc35332ab33d98
Aleksandr <sh7aleksandr@gmail.com>
2023-01-16 07:27:54 +0200

    v7

--- php1-laravel/app/Models/Category.php
+++ php1-laravel/app/Models/Category.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public function childs(){
+        return $this->hasMany(Category::class, 'paret');
+    }
+
+    public function parent(){
+        return $this->belongsTo(Category::class, 'paret');
+    }
 }
