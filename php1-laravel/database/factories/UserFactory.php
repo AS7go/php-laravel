6fef8426cc703283ba492f7c7a209144ee1c9b16
Aleksandr <sh7aleksandr@gmail.com>
2023-01-16 03:02:54 +0200

    create_permission_tables php1-laravel

--- php1-laravel/database/factories/UserFactory.php
+++ php1-laravel/database/factories/UserFactory.php
@@ -2,8 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -19,12 +22,25 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'surname' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
+            'phone' => fake()->unique()->e164PhoneNumber(),
+            'birthdate' => fake()->dateTimeBetween('-70 years', '-18 years')->format('Y-m-d'),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('test1234'), // password
+//            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
     }
+
+//    public function configure()
+//    {
+//        return $this->afterCreating(function (User $user) {
+//            $role = Role::findByName('name');
+//            dd($role);
+//        });
+//    }
+
 
     /**
      * Indicate that the model's email address should be unverified.
@@ -37,4 +53,14 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
+
+    public function withEmail(string $email) //створюємо самі не фєйкер
+    {
+        return $this->state(function (array $attrs) use ($email) {
+            return [
+                'email'=>$email
+            ];
+        });
+    }
+
 }
