e6d30c74304b508b25d242ee79e1659e8bc9deda
Aleksandr <sh7aleksandr@gmail.com>
2023-02-16 17:47:43 +0200

    hw26_v2_paypal

---
+++ php1-laravel/app/Http/Controllers/Payments/PaypalController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Payments;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PaypalController extends Controller
+{
+    public function create()
+    {
+
+    }
+
+    public function capture()
+    {
+
+    }
+
+    public function thankYou()
+    {
+
+    }
+}
