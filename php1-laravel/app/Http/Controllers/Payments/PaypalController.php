24e68079c2756b3a456082e69003ea2b310edecb
Aleksandr <sh7aleksandr@gmail.com>
2023-02-19 04:01:27 +0200

    id status

--- php1-laravel/app/Http/Controllers/Payments/PaypalController.php
+++ php1-laravel/app/Http/Controllers/Payments/PaypalController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Payments;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateOrderRequest;
 use App\Repositories\Contracts\OrderRepositoryContract;
-use Illuminate\Http\Request;
+use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Support\Facades\DB;
 use Srmklive\PayPal\Services\PayPal;
 
 class PaypalController extends Controller
@@ -21,7 +22,19 @@ class PaypalController extends Controller
 
     public function create(CreateOrderRequest $request, OrderRepositoryContract $repository)
     {
-        dd($request);
+        try {
+            DB::beginTransaction();
+            $total = Cart::instance('cart')->total();
+            $paypalOrder = $this->createPaymentOrder($total);
+            dd($paypalOrder);
+
+            \DB::commit();
+        }catch (\Exception $exception){
+            DB::rollBack();
+            logs()->warning($exception);
+
+            return response()->json(['error' => $exception->getMessage()], 422);
+        }
     }
 
     public function capture(string $vendorOrderId)
@@ -33,4 +46,20 @@ class PaypalController extends Controller
     {
 
     }
+
+    protected function createPaymentOrder($total): array
+    {
+        return $this->payPalClient->createOrder([
+            'intent' => 'CAPTURE',
+            'purchase_units' => [
+                [
+                    'amount' => [
+                        'currency_code' => config('paypal.currency'),
+                        'value' => $total
+                    ]
+                ]
+            ]
+        ]);
+    }
+
 }
